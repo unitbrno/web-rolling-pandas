5409d87753e58aff5695e17f8e3e3768c0b32e0e
dkozak <dkozak94@gmail.com>
2018-04-06 14:03:22 +0200

    add security config - disabled for now

---
+++ server/src/main/kotlin/rolling/pandas/server/SecurityConfig.kt
@@ -0,0 +1,24 @@
+package rolling.pandas.server
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("pass")
+                .roles("USER")
+    }
+}
